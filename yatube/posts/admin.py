1c0c8fa2dfc64ce8d2869c2733efd8e55bed0538
Валентин Климов <dd_apis@mail.ru>
2022-03-03 15:35:23 +0300

    hw02_community_v2 with corrections

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+
 from .models import Post
 from .models import Group
 
@@ -7,18 +8,18 @@ class PostAdmin(admin.ModelAdmin):
     list_editable = ('group',)
     list_display = ('pk', 'text', 'pub_date',
                     'author', 'group')
-# Перечисляем поля, которые должны
-# отображаться в админке
     search_fields = ('text',)
-    # Добавляем интерфейс для поиска по тексту постов
-    list_filter = ('pub_date',)  # Добавляем возможность фильтрации по дате
-    # Это свойство сработает для всех колонок: где пусто — там будет эта строка
+    list_filter = ('pub_date',)
     empty_value_display = '-пусто-'
-# При регистрации модели Post источником конфигурации для неё назначаем
-# класс PostAdmin
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'slug', 'description')
+    search_fields = ['title']
+    empty_value_display = '-пусто-'
 
 
 admin.site.register(Post, PostAdmin)
-admin.site.register(Group)
+admin.site.register(Group, GroupAdmin)
 
 # Register your models here.
--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -5,12 +5,16 @@ User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200,)
     slug = models.SlugField(unique=True)
     description = models.TextField()
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        ordering = ['title']
+        verbose_name_plural = 'Группы'
 
 
 class Post(models.Model):
@@ -24,8 +28,12 @@ class Post(models.Model):
     group = models.ForeignKey(
         Group,
         blank=True,
+        on_delete=models.SET_NULL,
         null=True,
-        on_delete=models.CASCADE,
         related_name='posts')
+
+    class Meta:
+        ordering = ['-pub_date']
+        verbose_name_plural = 'Записи блогов'
 
 # Create your models here.
